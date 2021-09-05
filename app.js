4580ee093e76d5c484ffed2a1b862c8f55ee079e
Mariano Lopez <marianolop22@yahoo.com.ar>
2021-09-05 15:49:57 -0300

    angular deploy

--- app.js
+++ app.js
@@ -11,35 +11,38 @@ hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 //Servir contenido estatico
 app.use( express.static('public'))
 
+// sec comenta esto ya que solo funciona para handelebars
+// app.get('/', (req,res)=>{
+//     res.render('home',{
+//         nombre: 'Mariano',
+//         titulo: 'Curso de node'
+//     })
+// })
 
-app.get('/', (req,res)=>{
-    res.render('home',{
-        nombre: 'Mariano',
-        titulo: 'Curso de node'
-    })
-})
+// app.get('/hola-mundo', (req,res)=>{
+//     res.send('Hola Mundo e su ruta');
+// })
 
-app.get('/hola-mundo', (req,res)=>{
-    res.send('Hola Mundo e su ruta');
-})
+// app.get('/generic', (req,res)=>{
+//     // res.sendFile( __dirname + '/public/generic.html'); //esto sirve el generic html pero sin mostrar el .html
+//     res.render('generic',{
+//         nombre: 'Mariano',
+//         titulo: 'Curso de node'
+//     })
+// })
+// app.get('/elements', (req,res)=>{
+//     // res.sendFile( __dirname + '/public/elements.html'); //esto sirve el generic html pero sin mostrar el .html
+//     res.render('elements',{
+//         nombre: 'Mariano',
+//         titulo: 'Curso de node'
+//     })
+// })
 
-app.get('/generic', (req,res)=>{
-    // res.sendFile( __dirname + '/public/generic.html'); //esto sirve el generic html pero sin mostrar el .html
-    res.render('generic',{
-        nombre: 'Mariano',
-        titulo: 'Curso de node'
-    })
-})
-app.get('/elements', (req,res)=>{
-    // res.sendFile( __dirname + '/public/elements.html'); //esto sirve el generic html pero sin mostrar el .html
-    res.render('elements',{
-        nombre: 'Mariano',
-        titulo: 'Curso de node'
-    })
-})
+
 app.get('*', (req,res)=>{
     // res.send('404 | page not fouund');
-    res.sendFile( __dirname + '/public/back/404.html') //con esto devuelvo una pagina de 404 not found
+    //res.sendFile( __dirname + '/public/back/404.html') //con esto devuelvo una pagina de 404 not found
+    res.sendFile( __dirname + '/public/index.html')
 })
 
 app.listen(port, ()=> {
